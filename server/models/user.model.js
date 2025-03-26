1ff53b1039f9a81fa57b2d86c999989bdca37ec2
Ubong Josiah <ubongjosiah@hotmail.com>
2025-03-25 22:50:43 -0400

    added user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,34 +1,17 @@
 const mongoose = require('mongoose');
-const EMAIL_REGEX = /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/;
-
-function emailValidation(val){
-    return EMAIL_REGEX.test(val);
-}
 
 const UserSchema = new mongoose.Schema({
-    nickname: { 
+    name: { 
         type: String,
         required: [
             true,
-            "Nickname is required"
-        ],
-        trim: true,
-    },
-    email: { 
-        type: String,
-        required: [
-            true,
-            "Email is required"
+            "Name is required"
         ],
         unique: [
             true,
-            "Email must be unique"
+            "Name must be unique"
         ],
-        trim: true,
-        validate: {
-            validator: val => emailValidation(val),
-            message: "Please enter a valid email"
-        }
+        trim: true
     }
 }, { timestamps: true });
 
